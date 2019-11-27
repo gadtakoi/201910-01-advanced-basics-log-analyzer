0fd2c375d74ce1086add656e9e37d61611569726
stas <ivanchenko@gambit.ru>
2019-11-27 10:04:01 +0300

    Fix 1

--- code/analyzer.py
+++ code/analyzer.py
@@ -1,42 +1,117 @@
+import os
+
 import gzip
 import json
 import logging
-import os
+from collections import namedtuple
 from statistics import median
 from string import Template
 
 
 class LogAnalyzer:
-    collected = {}
-    total_count = 0
-    request_time_total = 0
-    lines_with_error = 0
 
-    def __init__(self, config: dict, filedata: str):
+    def __init__(self, config: dict, filedata: namedtuple):
         self.config = config
         self.filedata = filedata
 
     def run(self):
         try:
             if not self.report_exists():
-                self.parse_log_file()
-                prepared = self.prepare_data()
+                parsed_log = LogParser(filedata=self.filedata).parse_log_file()
+                prepared = self.prepare_data(parsed_log=parsed_log)
                 sorted_urls = self.sort_and_slice(prepared=prepared)
                 self.create_report(log_data=sorted_urls)
-                self.error_threshold()
+                self.error_threshold(parsed_log=parsed_log)
         except KeyboardInterrupt:
             logging.exception('Нажатие CTRL-C')
 
     def report_exists(self) -> bool:
-        out = False
         report_name = '{}/report-{}.html'.format(
             self.config['REPORT_DIR'],
             self.filedata.date.replace('-', '.'))
-        if os.path.exists(report_name):
-            out = True
+        return os.path.exists(report_name)
+
+    def prepare_data(self, parsed_log: dict) -> dict:
+        out = dict()
+        for key, value in parsed_log['collected'].items():
+            count = value['count']
+            count_perc = round(count / (parsed_log['total_count'] / 100), 3)
+            time_sum = round(value['time_sum'], 3)
+            time_perc = round(
+                time_sum / (parsed_log['request_time_total'] / 100), 3)
+            time_avg = round(time_sum / count, 3)
+            time_max = value['time_max']
+            time_med = round(median(value['all_request_time']), 3)
+            item = {
+                'count': count,
+                'count_perc': count_perc,
+                'time_sum': time_sum,
+                'time_perc': time_perc,
+                'time_avg': time_avg,
+                'time_max': time_max,
+                'time_med': time_med,
+            }
+            out[key] = item
+
         return out
 
-    def parse_log_file(self):
+    def sort_and_slice(self, prepared: dict) -> list:
+        def localkey(k):
+            return k[1]['time_sum']
+
+        out = list()
+        for item, val in sorted(prepared.items(), key=localkey, reverse=True):
+            val['url'] = item
+            out.append(val)
+        return out[:self.config['REPORT_SIZE']]
+
+    def create_report(self, log_data: list):
+        report = ''
+        if log_data:
+            with open('template/report.html', 'r') as template_handler:
+                template = template_handler.read()
+                s = Template(template)
+                table_json = json.dumps(log_data)
+                report = s.safe_substitute(table_json=table_json)
+        if report:
+            report_name = '{}/report-{}.html'.format(
+                self.config['REPORT_DIR'],
+                self.filedata.date.replace('-', '.'))
+            with open(report_name, 'w') as report_handler:
+                try:
+                    report_handler.write(report)
+                except OSError:
+                    logging.exception('Нет места на диске')
+
+    def error_threshold(self, parsed_log: dict):
+        try:
+            percent = round(parsed_log['lines_with_error'] / (
+                    parsed_log['total_count'] / 100), 2)
+        except ZeroDivisionError:
+            percent = 0
+
+        if percent > 0:
+            try:
+                err_perc = self.config['ERROR_THRESHOLD']
+            except KeyError:
+                err_perc = 100
+
+            if percent > err_perc:
+                logging.info('Превышен порог ошибок: {}%'.format(str(percent)))
+            else:
+                logging.info('Ошибок: {}%'.format(str(percent)))
+
+
+class LogParser:
+    collected = {}
+    total_count = 0
+    lines_with_error = 0
+    request_time_total = 0
+
+    def __init__(self, filedata: namedtuple):
+        self.filedata = filedata
+
+    def parse_log_file(self) -> dict:
         if self.filedata:
             open_file = gzip.open if self.filedata.ext == 'gz' else open
             with open_file(self.filedata.path, 'rt') as file_handler:
@@ -44,6 +119,12 @@ class LogAnalyzer:
                     parsed_line = self.parse_line(line=line)
                     self.data_collector(line=parsed_line)
                     self.total_count += 1
+        return {
+            'collected': self.collected,
+            'total_count': self.total_count,
+            'request_time_total': self.request_time_total,
+            'lines_with_error': self.lines_with_error,
+        }
 
     def read_line(self, file_object):
         while True:
@@ -109,74 +190,3 @@ class LogAnalyzer:
                 }
                 self.request_time_total += request_time
                 self.collected[line[0]] = inside
-
-    def prepare_data(self) -> dict:
-        out = dict()
-        for key, value in self.collected.items():
-            count = value['count']
-            count_perc = round(count / (self.total_count / 100), 3)
-            time_sum = round(value['time_sum'], 3)
-            time_perc = round(time_sum / (self.request_time_total / 100), 3)
-            time_avg = round(time_sum / count, 3)
-            time_max = value['time_max']
-            time_med = round(median(value['all_request_time']), 3)
-            item = {
-                'count': count,
-                'count_perc': count_perc,
-                'time_sum': time_sum,
-                'time_perc': time_perc,
-                'time_avg': time_avg,
-                'time_max': time_max,
-                'time_med': time_med,
-            }
-            out[key] = item
-
-        self.collected = dict()
-
-        return out
-
-    def sort_and_slice(self, prepared: dict) -> list:
-        def localkey(k):
-            return k[1]['time_sum']
-
-        out = list()
-        for item, value in sorted(prepared.items(),
-                                  key=localkey,
-                                  reverse=True):
-            value['url'] = item
-            out.append(value)
-        return out[:self.config['REPORT_SIZE']]
-
-    def create_report(self, log_data: list):
-        if log_data:
-            with open('template/report.html', 'r') as template_handler:
-                template = template_handler.read()
-                s = Template(template)
-                table_json = json.dumps(log_data)
-                report = s.safe_substitute(table_json=table_json)
-                if report:
-                    report_name = '{}/report-{}.html'.format(
-                        self.config['REPORT_DIR'],
-                        self.filedata.date.replace('-', '.'))
-                    with open(report_name, 'w') as report_handler:
-                        try:
-                            report_handler.write(report)
-                        except OSError:
-                            logging.exception('Нет места на диске')
-
-    def error_threshold(self):
-        try:
-            perc = round(self.lines_with_error / (self.total_count / 100), 2)
-        except ZeroDivisionError:
-            perc = 0
-
-        if perc > 0:
-            try:
-                err_perc = self.config['ERROR_THRESHOLD']
-            except KeyError:
-                err_perc = 100
-
-            if perc > err_perc:
-                logging.info('Превышен порог ошибок: {}%'.format(str(perc)))
-            else:
-                logging.info('Ошибок: {}%'.format(str(perc)))
--- log_analyzer.py
+++ log_analyzer.py
@@ -6,12 +6,16 @@
 #                     '$status $body_bytes_sent "$http_referer" '
 #                     '"$http_user_agent" "$http_x_forwarded_for" "$http_X_REQUEST_ID" "$http_X_RB_USER" '
 #                     '$request_time';
-import argparse
+import os
+import re
 import logging
+import argparse
+import datetime
+
+from collections import namedtuple
+from importlib import import_module
 
 from code.analyzer import LogAnalyzer
-from code.config import LogAnalyzerConf
-from code.last_log import LastLog
 
 config = {
     "REPORT_SIZE": 1000,
@@ -20,18 +24,76 @@ config = {
 }
 
 
+def get_config(default_config: dict, config_file: str) -> dict:
+    result_config = default_config
+    module_name = os.path.splitext(config_file)[0]
+    try:
+        config = import_module(module_name)
+        result_config = dict(default_config, **config.config)
+    except ModuleNotFoundError:
+        raise ModuleNotFoundError('Конфигурационный файл {} не найден: '
+                                  ''.format(config_file))
+    except NameError:
+        raise AttributeError('Ошибка в конфигурационном '
+                             'файле: {}'.format(config_file))
+    except AttributeError:
+        pass
+    except SyntaxError:
+        raise SyntaxError('Синтаксическая ошибка в конфигурационном '
+                          'файле: {}'.format(config_file))
+
+    if 'ERROR_LOG' not in result_config:
+        result_config['ERROR_LOG'] = None
+
+    return result_config
+
+
+def get_last_log(config: dict) -> namedtuple:
+    files = []
+    log_dates = []
+    log_files = {}
+    FileData = namedtuple('FileData', 'path date ext')
+
+    for filename in os.listdir(config['LOG_DIR']):
+        files.append(filename)
+
+    for file in files:
+        log_date_str = re.match(
+            r'^nginx-access-ui\.log-(?P<date>\d{8})(\.gz)?$', file)
+        if log_date_str:
+            try:
+                log_date = datetime.datetime.strptime(
+                    log_date_str.group('date'), "%Y%m%d").date()
+                log_dates.append(log_date)
+                log_files[str(log_date)] = file
+            except ValueError:
+                logging.info('Некорректная дата лог файла {}'.format(
+                    log_date_str.group()))
+                pass
+
+    if log_dates:
+        sorted_log_dates = sorted(log_dates)
+        log_date = sorted_log_dates[-1]
+
+        filedate = str(log_date)
+        fileext = log_files[filedate].split('.')[-1]
+        filepath = "{}/{}".format(config['LOG_DIR'], log_files[filedate])
+        filedata = FileData(filepath, filedate, fileext)
+
+        return filedata
+
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--config', default='config.py', help='Config file')
     args = parser.parse_args()
 
-    conf = LogAnalyzerConf().get_config(default_config=config,
-                                        config_file=args.config)
+    conf = get_config(default_config=config, config_file=args.config)
 
     logging.basicConfig(filename=conf['ERROR_LOG'],
                         format='[%(asctime)s] %(levelname).1s %(message)s',
                         level=logging.DEBUG)
-    lastlog = LastLog().get_last_log(config=conf)
+    lastlog = get_last_log(config=conf)
     if lastlog:
         LogAnalyzer(config=conf, filedata=lastlog).run()
     else:
